5810bff8fcfb46678a894dee70c1f08079a481a6
ColtAuron <127497083+ColtAuron@users.noreply.github.com>
2024-04-15 15:15:33 +0800

    yes

---
+++ DbConfig.py
@@ -0,0 +1,130 @@
+import sqlite3
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "bimData.db")
+con = sqlite3.connect(db_path)
+c = con.cursor()
+
+command = ('''CREATE TABLE IF NOT EXISTS Animal(
+            AnimalID INTEGER PRIMARY KEY,
+            Name Varchar(30),
+            sciName Varchar(50),
+            desc Varchar(500),
+            img Varchar(100)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS Plant(
+            PlantID INTEGER PRIMARY KEY,
+            Name Varchar(30),
+            sciName Varchar(50),
+            desc Varchar(500),
+            img Varchar(100)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS Biome (
+            BiomeID INTEGER PRIMARY KEY,
+            Name Varchar(30),
+            desc Varchar(500),
+            img Varchar(100)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS Tourist (
+            TouristID INTEGER PRIMARY KEY,
+            Name Varchar(30),
+            link Varchar(50),
+            desc Varchar(500),
+            img Varchar(100)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS City (
+            CityID INTEGER PRIMARY KEY,
+            Name Varchar(50)          
+)''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS AnimalsLoc (
+            ID INTEGER PRIMARY KEY,
+            AnimalID INTEGER,
+            CityID INTEGER,
+            xPos FLOAT(3, 7),
+            yPos FLOAT(3, 7),
+            FOREIGN KEY(AnimalID) REFERENCES Animal(AnimalID),
+            FOREIGN KEY(CityID) REFERENCES City(CityID)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS PlantsLoc (
+            ID INTEGER PRIMARY KEY,
+            PlantsID INTEGER,
+            CityID INTEGER,
+            xPos FLOAT(3, 7),
+            yPos FLOAT(3, 7),
+            FOREIGN KEY(PlantsID) REFERENCES Plant(ID),
+            FOREIGN KEY(CityID) REFERENCES City(ID)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS BiomeLoc (
+            ID INTEGER PRIMARY KEY,
+            BiomeID INTEGER,
+            CityID INTEGER,
+            xPos FLOAT(3, 7),
+            yPos FLOAT(3, 7),
+            FOREIGN KEY(BiomeID) REFERENCES Biome(ID),
+            FOREIGN KEY(CityID) REFERENCES City(ID)
+            )''')
+
+c.execute(command)
+
+command = ('''CREATE TABLE IF NOT EXISTS TouristLoc (
+            ID INTEGER PRIMARY KEY,
+            TouristID INTEGER,
+            CityID INTEGER,
+            xPos FLOAT(3, 7),
+            yPos FLOAT(3, 7),
+            FOREIGN KEY(TouristID) REFERENCES Tourist(ID),
+            FOREIGN KEY(CityID) REFERENCES City(ID)
+            )''')
+
+c.execute(command)
+
+# cities = [("Nasugbu",),("Lian",), ("Tuy",), ("Calatagan",), ("Balayan",), ("Calaca",), ("Lemery",),
+#           ("Laurel",), ("Agoncillo",), ("Taal",), ("San Luis",), ("Bauan",), ("Mabini",), ("Tingloy",),
+#           ("Sta. Teresita",), ("Talisay",), ("San Nicolas",), ("Alitagtag",), ("San Pascual",),
+#           ("Cuenca",), ("San Jose",), ("Tanauan",), ("Malvar",), ("Balete",), ("Mataas na Kahoy",),
+#           ("Lipa",), ("Ibaan",), ("Sto. Tomas",), ("Padre Garcia",), ("Rosario",), ("Taysan",),
+#           ("Lobo",), ("San Juan",)]
+
+# c.executemany("INSERT INTO CITY(Name) VALUES(?)", cities)
+
+# c.execute("INSERT INTO Animal VALUES (1, 'Parrot', 'berb', 'a bird', 'picture.png')")
+
+# c.execute("INSERT INTO AnimalsLoc Values (1, 1, 1, 13.7627346, 121.0569921)")
+
+#c.execute("INSERT INTO PLANT VALUES (1, 'Sunflower', 'araw', 'a flower', 'sunflower.png')")
+
+#c.execute("INSERT INTO PlantsLoc VALUES (1, 1, 1, 13.7647354, 121.0602537)")
+
+#c.execute("INSERT INTO Tourist VALUES (1, 'Swimming pool', 'www.youtube.com', 'for swimming', 'pool.png')")
+
+#c.execute("INSERT INTO TouristLoc VALUES (1, 1, 1, 13.7620260, 121.0685793)")
+
+#c.execute("INSERT INTO Biome VALUES (1, 'River', 'Waterflow', 'river.png')")
+
+# c.execute("INSERT INTO BiomeLoc VALUES (1, 1, 1, 13.7577326, 121.0643736)")
+
+# c.execute("SELECT * FROM BiomeLoc")
+# print(c.fetchall())
+
+con.commit()
--- backend.py
+++ backend.py
@@ -1,56 +1,164 @@
-#animals
-#tourist destinations
-#types of biomes
-#plants
 import tkinter
 import tkintermapview
+import os
+import sqlite3
 
-tk = tkinter.Tk()
-tk.geometry(f"{800}x{600}")
-
-map_widget = tkintermapview.TkinterMapView(tk, width=800, height= 600, corner_radius=0)
-map_widget.place(relx=.5, rely=.5, anchor=tkinter.CENTER)
-map_widget.set_position(13.8525866, 121.0435568)
-map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
-map_widget.set_zoom(10)
-
-tk.mainloop()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "bimData.db")
+con = sqlite3.connect(db_path)
+c = con.cursor()
 
 class Animals:
-    def __init__(self, Name, sciName, desc, xPos, yPos, img):
+    all = []
+    def __init__(self, Name, sciName, desc, xPos, yPos, img, city, type = "Animal", disabled=False):
         self.Name = Name
         self.sciName = sciName
         self.desc = desc
         self.xPos = xPos
         self.yPos = yPos
         self.img = img
+        self.city = city
+        self.type = type
+        self.disabled = disabled
+        Animals.all.append(self)
+
+animal_table = []
+c.execute("Select Animal.Name, sciName, desc, img, City.Name, xPos, yPos From AnimalsLoc, Animal, City WHERE AnimalsLoc.AnimalID = Animal.AnimalID AND AnimalsLoc.CityID = City.CityID")
+frSql = c.fetchall()
+for items in frSql:
+    Animals(items[0], items[1], items[2], items[5], items[6], items[3], items[4])
 
 class Plants:
-    def __init__(self, Name, sciName, desc, xPos, yPos, img):
+    all = []
+    def __init__(self, Name, sciName, desc, xPos, yPos, img, city, type = "Plant", disabled = False):
         self.Name = Name
         self.sciName = sciName
         self.desc = desc
         self.xPos = xPos
         self.yPos = yPos
         self.img = img
+        self.city = city
+        self.type = type
+        self.disabled = disabled
+        self.all.append(self)
+
+plant_table = []
+c.execute("Select Plant.Name, sciName, desc, img, City.Name, xPos, yPos From PlantsLoc, Plant, City WHERE PlantsLoc.PlantsID = Plant.PlantID AND PlantsLoc.CityID = City.CityID")
+frSql = c.fetchall()
+for items in frSql:
+    Plants(items[0], items[1], items[2], items[5], items[6], items[3], items[4])
+
 
 class TouristDes:
-    def __init__(self, Name, link, desc, xPos, yPos, img):
+    all = []
+    def __init__(self, Name, link, desc, xPos, yPos, img, city, type = "Tourist Destination", disabled = False):
         self.Name = Name
         self.link = link
         self.desc = desc
         self.xPos = xPos
         self.yPos = yPos
         self.img = img
+        self.city = city
+        self.type = type
+        self.disabled = disabled
+        self.all.append(self)
+
+Tour_table = []
+c.execute("Select Tourist.Name, link, desc, img, City.Name, xPos, yPos From TouristLoc, Tourist, City WHERE TouristLoc.TouristID = Tourist.TouristID AND TouristLoc.CityID = City.CityID")
+frSql = c.fetchall()
+for item in frSql:
+    TouristDes(item[0], item[1], item[2], item[5], item[6], item[3], item[4])
 
 class Biomes:
-    def __init__(self, Name, type, desc, xPos, yPos, img):
+    all=[]
+    def __init__(self, Name, desc, xPos, yPos, img, city, type = 'Biome', disabled = False):
         self.Name = Name
-        self.type = type
         self.desc = desc
         self.xPos = xPos
         self.yPos = yPos
         self.img = img
+        self.city = city
+        self.disabled = disabled
+        self.all.append(self)
+
+Biome_table = []
+c.execute("Select Biome.Name, desc, img, City.Name, xPos, yPos From BiomeLoc, Biome, City WHERE BiomeLoc.BiomeID = Biome.BiomeID AND BiomeLoc.CityID = City.CityID")
+frSql = c.fetchall()
+for item in frSql:
+    Biomes(item[0], item[1], item[4], item[5], item[2], item[3])
 
 
+class App():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.tk = tkinter.Tk()
+        self.tk.geometry(f"{800}x{600}")  
+
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        database_path = os.path.join(script_directory, "batangas.db")
+
+        self.map_widget = tkintermapview.TkinterMapView(self.tk, width=800, height= 600, database_path=database_path, corner_radius=0)
+        self.map_widget.place(relx=.5, rely=.5, anchor=tkinter.CENTER)
+        #13.8525866, 121.0435568
+        self.map_widget.set_position(deg_x=13.7582328,deg_y=121.0726133)
+        self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+        self.map_widget.set_zoom(13)
+
+        self.animalMarkers = []
+        self.animalInfo = [] 
+        self.plantMarkers = []
+        self.plantInfo = []
+        self.touristMarkers = []
+        self.touristInfo = []
+        self.biomeMarkers = []
+        self.biomeInfo = []
+        
+        for item in Animals.all:
+            self.animalMarkers.append(self.map_widget.set_marker(item.xPos, item.yPos, item.Name, command=self.animal_active),)
+            self.animalInfo.append([item.sciName, item.desc, item.img, item.city])
+        for item in Plants.all:
+            self.plantMarkers.append(self.map_widget.set_marker(item.xPos, item.yPos, item.Name, command=self.plant_active),)
+            self.plantInfo.append([item.sciName, item.desc, item.img, item.city])
+        for item in TouristDes.all:
+            self.plantMarkers.append(self.map_widget.set_marker(item.xPos, item.yPos, item.Name, command=self.tourist_active),)
+            self.plantInfo.append([item.link, item.desc, item.img, item.city])
+        for item in Biomes.all:
+            self.biomeMarkers.append(self.map_widget.set_marker(item.xPos,item.yPos, item.Name, command=self.biome_active),)
+            self.biomeInfo.append([item.desc, item.img, item.city])
+
+    def animal_active(self, marker):
+        sciName = self.animalInfo[self.animalMarkers.index(marker)][0]
+        desc = self.animalInfo[self.animalMarkers.index(marker)][1]
+        img = self.animalInfo[self.animalMarkers.index(marker)][2]
+        city = self.animalInfo[self.animalMarkers.index(marker)][3]
+        print(sciName, desc, img, city)
+    
+    def plant_active(self, marker):
+        sciName = self.plantInfo[self.plantMarkers.index(marker)][0]
+        desc = self.plantInfo[self.plantMarkers.index(marker)][1]
+        img = self.plantInfo[self.plantMarkers.index(marker)][2]
+        city = self.plantInfo[self.plantMarkers.index(marker)][3]
+        print(sciName, desc, img, city)
+    
+    def tourist_active(self, marker):
+        link = self.plantInfo[self.plantMarkers.index(marker)][0]
+        desc = self.plantInfo[self.plantMarkers.index(marker)][1]
+        img = self.plantInfo[self.plantMarkers.index(marker)][2]
+        city = self.plantInfo[self.plantMarkers.index(marker)][3]
+        print(link, desc, img, city)
+
+    def biome_active(self, marker):
+        desc = self.biomeInfo[self.biomeMarkers.index(marker)][0]
+        img = self.biomeInfo[self.biomeMarkers.index(marker)][1]
+        city = self.biomeInfo[self.biomeMarkers.index(marker)][2]
+        print(desc, img, city)
+
+    def start(self):
+        self.tk.mainloop()
+    
+#Remove this if Implement to Front
+if __name__ == "__main__":
+    app = App()
+    app.start()
 
