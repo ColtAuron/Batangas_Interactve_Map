8973ac9530a5cfafbd50b9c31c6c2c69a486fb3f
barskeai <98462194+barskeai@users.noreply.github.com>
2024-05-05 20:28:09 +0800

    UPDATED THE TOURIST LLOCATIONS
    goods na sya hehehehe

--- NEW_DB.py
+++ NEW_DB.py
@@ -529,17 +529,17 @@ tourist_loc = [
 
 
 
-c.executemany("INSERT INTO City VALUES (?,?,?,?,?,?,?,?)", cities)
+# c.executemany("INSERT INTO City VALUES (?,?,?,?,?,?,?,?)", cities)
 
-c.executemany("INSERT INTO Animal VALUES (?,?,?,?,?,?,?)", animals)
+# c.executemany("INSERT INTO Animal VALUES (?,?,?,?,?,?,?)", animals)
 
-c.executemany("INSERT INTO Plant VALUES (?,?,?,?,?,?)", plants)
+# c.executemany("INSERT INTO Plant VALUES (?,?,?,?,?,?)", plants)
 
-c.executemany("INSERT INTO Tourist VALUES (?,?,?,?,?,?)", tourist_spots)
+# c.executemany("INSERT INTO Tourist VALUES (?,?,?,?,?,?)", tourist_spots)
 
-c.executemany("INSERT INTO Cities_Loc VALUES (?,?,?,?)", cities_loc)
+# c.executemany("INSERT INTO Cities_Loc VALUES (?,?,?,?)", cities_loc)
 
-c.executemany("INSERT INTO Tourist_Loc VALUES (?,?,?,?,?)", tourist_loc)
+# c.executemany("INSERT INTO Tourist_Loc VALUES (?,?,?,?,?)", tourist_loc)
 
 print('Command executed successfully!!!')
 
